de17b0c59379893f7a1a28ac756cb7c72ccb8c94
Katarios2015 <prichal7@bk.ru>
2024-10-24 14:48:12 +0300

    Последнее испытание (часть 1)

--- src/components/place-card/place-card.test.tsx
+++ src/components/place-card/place-card.test.tsx
@@ -1,18 +1,24 @@
 import {render, screen} from '@testing-library/react';
 import {withHistory, withStore} from '../../utils/mock-component';
 import PlaceCard from './place-card';
-import {makeFakeOfferCard} from '../../utils/moks';
+import {makeFakeOfferCard, makeFakeStore} from '../../utils/moks';
+import {AuthorizationStatus} from '../../store/const';
 
 
-describe('Component: FavoritePlaceCard', () => {
+describe('Component: PlaceCard', () => {
   it('should render correct', () => {
     const fakeOffer = makeFakeOfferCard();
-
     const placeCardValueTestId = 'placeCard';
 
-    const { withStoreComponent } = withStore(<PlaceCard offer={fakeOffer}/>, {});
+    const withHistoryComponent = withHistory(<PlaceCard offer={fakeOffer}/>);
+    const { withStoreComponent } = withStore(withHistoryComponent, makeFakeStore({ USER: {
+      authorizationStatus: AuthorizationStatus.NoAuth,
+      user: null,
+      isLoginFormDasabled: false,
+      email: ''
+    } }));
 
-    const preparedComponent = withHistory(withStoreComponent);
+    const preparedComponent = withStoreComponent;
     render(preparedComponent);
 
     const placeCardValues = screen.getAllByTestId(placeCardValueTestId);
